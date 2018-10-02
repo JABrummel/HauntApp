ae57e6f5c6ce97afb2ea753ec1fb4a83f7a97981
JABrummel <43587796+JABrummel@users.noreply.github.com>
2018-10-02 15:23:56 -0400

    start of listed events and create events activity

--- src/main/java/com/example/jessb/haunt/CreateEvent.java
+++ src/main/java/com/example/jessb/haunt/CreateEvent.java
@@ -1,13 +1,35 @@
 package com.example.jessb.haunt;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class CreateEvent extends AppCompatActivity {
+
+    TextView datePicked;
+    TextView startTime;
+    TextView endTime;
+    DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        datePicked = (TextView)findViewById(R.id.tv_date);
+
+        datePicked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DATE);
+            }
+        });
     }
 }
--- src/main/java/com/example/jessb/haunt/ListedEvents.java
+++ src/main/java/com/example/jessb/haunt/ListedEvents.java
@@ -1,7 +1,11 @@
 package com.example.jessb.haunt;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListedEvents extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class ListedEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listed_events);
+        FloatingActionButton moreButton = findViewById(R.id.button_more);
+        FloatingActionButton addButton = findViewById(R.id.button_add);
+        Intent lastActivity = getIntent();
+        String usertype = lastActivity.getStringExtra("user");
+
+        if(usertype.equals("student")) {
+
+            addButton.hide();
+            moreButton.hide();
+        }
+
+        if(usertype.equals("club")) {
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    createEvent();
+                }
+            });
+
+        }
+
+
+    }
+
+    protected void createEvent(){
+        Intent newEvent = new Intent(this, CreateEvent.class);
+        startActivity(newEvent);
     }
 }
